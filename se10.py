d77ec9cc339be7f7951718cf5091bf54c074b366
Isaac <jannealvillamin@gmail.com>
2022-10-05 22:56:26 +0800

    First commit

--- se10.py
+++ se10.py
@@ -1,7 +1,7 @@
 n = int(input())
 for i in range(n):
     for j in range(2 * n - 1):
-        if n - i - 1 <= j and j < n + i:
+        if n - 1 - i <= j and j <= n - 1 + i:
             print('#', end='')
         else:
             print(' ', end='')
